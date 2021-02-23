15c00aaff7568a15a7ecb1667d562db31e4f46a4
FabianCR27 <47484275+FabianCR27@users.noreply.github.com>
2021-02-23 14:34:14 -0800

    PlatformAdventure

---
+++ Assets/Scrips/scoringtext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scoringtext : MonoBehaviour
+{
+    public GameObject textii;
+    public static int thescore;
+   
+
+     void Update()
+    {
+        
+        textii.GetComponent<Text>().text = "Score :" + thescore;
+    }
+}
